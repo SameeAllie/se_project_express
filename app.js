0bbab9a12ac9c189dd56ecb939ac1111a4efe1d5
Json Todd <98905482+SameeAllie@users.noreply.github.com>
2023-08-31 14:07:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -10,14 +10,20 @@ mongoose.set("strictQuery", false);
 const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
+// Add the crash-test route before the signin and signup routes
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+
 const routes = require("./routes");
 
 app.use(express.json());
 app.use(cors());
+
 app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App is listening at port ${PORT}`);
 });
-
-app.use(cors());
